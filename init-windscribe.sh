2ab274e207debf32c3d772b51077622a197809e8
Simon DENEL <sdenel@smartwavesa.com>
2019-04-22 21:03:50 +0200

    Set text in bold

--- init-windscribe.sh
+++ init-windscribe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-echo "Starting Windscribe..."
+echo -e "\033[1mStarting Windscribe...\033[0m"
 /etc/init.d/windscribe-cli start
-echo -e "\033[1mPlease login to Windscribe. Don't have an account yet? The free offer includes 10Go/month. Go to: https://windscribe.com/?affid=06phaj3u (affiliated link)[0m"
+echo -e "\033[1mPlease login to Windscribe. Don't have an account yet? The free offer includes 10Go/month. Go to: https://windscribe.com/?affid=06phaj3u (affiliated link)\033[0m"
 windscribe login
-echo "Trying to connect..."
+echo -e "\033[1mTrying to connect...\033[0m"
 windscribe connect
