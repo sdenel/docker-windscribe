0f3e37fe89f5ce9dce0c3f104e2550cd5a052712
Simon DENEL <sdenel@smartwavesa.com>
2019-04-22 19:56:43 +0200

    Fix typo

--- init-windscribe.sh
+++ init-windscribe.sh
@@ -2,7 +2,7 @@
 set -e
 echo "Starting Windscribe..."
 /etc/init.d/windscribe-cli start
-echo "Please ogin to Windscribe. Don't have an account yet? The free offer includes 10Go/month. Go to: https://windscribe.com/?affid=06phaj3u (affiliated link)"
+echo "Please login to Windscribe. Don't have an account yet? The free offer includes 10Go/month. Go to: https://windscribe.com/?affid=06phaj3u (affiliated link)"
 windscribe login
 echo "Trying to connect..."
 windscribe connect
