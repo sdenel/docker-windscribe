606c15ab66764e431304c2abe698e6d8b35b0156
Simon DENEL <sdenel@smartwavesa.com>
2019-04-24 19:05:04 +0200

    Add firewall mode

--- init-windscribe.sh
+++ init-windscribe.sh
@@ -6,3 +6,5 @@ echo -e "\033[1mPlease login to Windscribe. Don't have an account yet? The free 
 windscribe login
 echo -e "\033[1mTrying to connect...\033[0m"
 windscribe connect
+echo -e "\033[1mEnable firewall mode: You will not have any internet connectivity if you are not connected to Windscribe.\0330m"
+windscribe firewall on
